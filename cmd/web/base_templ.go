ebfaffdbe242210641e172dceb1cc1f5d1d5be50
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2024-05-05 18:54:39 +0200

    more variables

--- cmd/web/base_templ.go
+++ cmd/web/base_templ.go
@@ -23,7 +23,7 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Dominic A. R. Gerhauser</title><script src=\"/js/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/css/main.css\"></head><body class=\"grid-pancake ut-min-h-100\"><header>Ich - Kontakt</header><main class=\"ut-center\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Dominic A. R. Gerhauser</title><script src=\"/js/htmx.min.js\"></script><link rel=\"stylesheet\" href=\"/css/main.css\"></head><body class=\"grid-pancake ut-min-h-100\"><header class=\"\">Ich - Kontakt</header><main class=\"ut-center\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
